a5a79485a8b44dca5943d40b780661a9cbf055ef
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2022-05-22 19:11:54 -0300

    Definimos el metodo constructor, los metodos getUser y getPsw, y el metodo verificarHeadersEnServer de la clase AuthToken

--- Class/AuthToken.php
+++ Class/AuthToken.php
@@ -3,13 +3,33 @@ require_once "Models/Token.php";
 require_once "Models/User.php";
 
 class AuthToken {
+    private $user;
+    private $psw;
 
-    public function verificarCredenciales() {
+    public function __construct($headers=['user'=>null,'psw'=>null])
+    {
+        if($this->verificarHeadersEnServer($headers)) {
+            $this->user = $_SERVER[$headers['user']];
+            $this->psw  = $_SERVER[$headers['psw']];
+        }
+        else {
+            die('No existen esos encabezados en $_SERVER');
+        }
+    }
 
+    public function verificarCredencialesEnDB() {
+        
     }
 
     public function verificarMetodoHTTP() {
-
+        switch($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                break;
+            case 'POST':
+                break;
+            default:
+                die;
+        };
     }
 
     public function get() {
@@ -22,5 +42,22 @@ class AuthToken {
 
     public function run() {
 
+    }
+
+    public function getUser() {
+        return $this->user;
+    }
+
+    public function getPsw() {
+        return $this->psw;
+    }
+
+    public function verificarHeadersEnServer($headers) {
+        if(array_key_exists($headers['user'], $_SERVER) && array_key_exists($headers['psw'], $_SERVER)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
