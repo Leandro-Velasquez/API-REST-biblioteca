2d17b3b1a0bc1322d2d70a2db45f0edc73663306
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2022-05-22 18:15:21 -0300

    Creamos la carpeta Class, Creamos el archivo AuthToken.php, declaramos la clase AuthToken y algunos de sus metodos

---
+++ Class/AuthToken.php
@@ -0,0 +1,26 @@
+<?php
+require_once "Models/Token.php";
+require_once "Models/User.php";
+
+class AuthToken {
+
+    public function verificarCredenciales() {
+
+    }
+
+    public function verificarMetodoHTTP() {
+
+    }
+
+    public function get() {
+
+    }
+
+    public function post() {
+
+    }
+
+    public function run() {
+
+    }
+}
