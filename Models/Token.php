591788b700f80596f05535b0420ab911b59b5ccc
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2022-05-22 21:17:27 -0300

    Definimos el metodo getToken de la clase Token

--- Models/Token.php
+++ Models/Token.php
@@ -2,6 +2,15 @@
 require_once "Db.php";
 
 class Token {
+    public static $table = 'tokens';
+
+    public static function getToken($token) {
+        $sql = 'SELECT * FROM ' . self::$table . ' WHERE token=?';
+        $stmt = Db::connect()->prepare($sql);
+        $stmt->execute(array($token));
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 
     public static function generateToken($id, $user) {
         return sha1($id . $user. time());
