088cde0976fd2ee719a3d16536e5c66817699515
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-05-31 18:36:31 -0300

    Agregamos codigos de estado al metodo post de BooksService

--- Services/BooksService.php
+++ Services/BooksService.php
@@ -5,15 +5,21 @@ class BooksService {
 
     public function get($id = null) {
         $books = !empty($id) ? Book::selectById($id): Book::selectAll();
-        header('Content-type: application/json');
-        echo json_encode($books);
+        if($books){
+            header('Content-type: application/json');
+            echo json_encode($books);
+            http_response_code(200);
+        }else {
+            http_response_code(404);
+        }
+        
     }
 
     public function post() {
         $dir_subida = 'Images/uploads/';
         $imagen_subida = $dir_subida . basename($_FILES['file']['name']);
 
-        $data = array('nombre' => $_POST['nombre'], 'autor' => $_POST['autor'], 'imagen' => $dir_subida);
+        $data = array('nombre' => $_POST['nombre'], 'autor' => $_POST['autor'], 'imagen' => $imagen_subida);
         if(move_uploaded_file($_FILES['file']['tmp_name'], $imagen_subida)) {
             Book::addBook($data);
             $json = array('id' => Book::getIdLastBookAdd()['id_libro'], 'nombre' => $_POST['nombre'], 'autor' => $_POST['autor'], 'imagen' => $_FILES['file']['name']);
